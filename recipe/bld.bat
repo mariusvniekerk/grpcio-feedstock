77d556476d2bb51213468b9200f498e7fce14f25
Marius van Niekerk <marius.v.niekerk@gmail.com>
2022-06-14 10:53:15 -0400

    win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,7 @@ if "%ARCH%" == "32" (set PLATFORM=x86) else (set PLATFORM=x64)
 set "GRPC_PYTHON_BUILD_SYSTEM_ZLIB=True"
 set "GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=True"
 set "GRPC_PYTHON_CFLAGS=/DPB_FIELD_16BIT"
+set "GRPC_PYTHON_BUILD_SYSTEM_GRPC_CORE=True"
 
 :: clearing this regenerates it with cython and fixes phtread usage
 del src\python\grpcio\grpc\_cython\cygrpc.cpp
